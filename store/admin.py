6823df073577edcdd979bbb91a35ed860acba94f
r-elsa <elsaryynanen@outlook.com>
2024-07-02 11:06:48 +0300

    changes to database schema and configuration

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Category, Product
+
+@admin.register(Category) # tell django that we want to register model
+class CategoryAdmin(admin.ModelAdmin): # give it more instructions
+    list_display= ['name', 'slug' ] # tel Django what fields to show in he list 
+    prepopulated_fields={'slug': ('name',)}
+
+
+@admin.register(Product) # tell django that we want to register model
+class ProductAdmin(admin.ModelAdmin): # give it more instructions
+    list_display= ['title','author', 'slug', 'price', 'in_stock', 'created', 'updated' ] # tel Django what fields to show in he list 
+    list_filter=['in_stock', 'is_active']
+    list_editable=['price', 'in_stock']
+    prepopulated_fields={'slug': ('title',)}
--- store/models.py
+++ store/models.py
@@ -27,7 +27,8 @@ class Product(models.Model):
     price=models.DecimalField(max_digits=4, decimal_places=2)
     in_stock=models.BooleanField(default=True)
     is_active=models.BooleanField(default=True) # active to buy
-    created=models.DateTimeField(auto_now=True)
+    created=models.DateTimeField(auto_now_add=True)
+    updated=models.DateTimeField(auto_now=True)
 
 
 
