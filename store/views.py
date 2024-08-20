46ba175c2dfbca148471df298e572455f31d51d8
r-elsa <elsaryynanen@outlook.com>
2024-08-20 21:37:01 +0300

    Categories -dropdown works and products are displayed

--- store/views.py
+++ store/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import Category, Product
 
 from django.http import HttpResponse
 
 
+def all_products(request):
+    products= Product.objects.all()
+    return render(request, 'store/home.html', {'products':products})
+
+
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+def categories(request):
+    return {
+        'categories': Category.objects.all()
+
+    }
