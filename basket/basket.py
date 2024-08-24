650a272d9fb744236d1411d3fc40f6b0f72ad0aa
r-elsa <elsaryynanen@outlook.com>
2024-08-24 15:23:14 +0300

    basket qty working

--- basket/basket.py
+++ basket/basket.py
@@ -5,5 +5,14 @@ class Basket ():
         basket = self.session.get('skey')
 
         if 'skey' not in request.session:
-            basket = self.session['skey']= {'number':  34332233}
+            basket = self.session['skey']= {}
         self.basket = basket
+
+    
+    def add(self, product, product_qty):
+        product_id= product.id
+
+        if product_id not in self.basket:
+            self.basket[product_id]= {'price': str(product.price), 'qty': int(product_qty)}
+        
+        self.session.modified = True
--- basket/views.py
+++ basket/views.py
@@ -1,7 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import JsonResponse
+from .basket import Basket
+from store.models import Product
 
 # Create your views here.
 
 def basket_summary(request):
     return render(request, 'store/basket/summary.html')
+
+def basket_add(request):
+    basket = Basket(request)
+    if request.POST.get('action') == 'post':
+        product_id = int(request.POST.get('product_id'))
+        product_qty = int(request.POST.get('product_qty')) 
+        product = get_object_or_404(Product, id=product_id)
+        basket.add(product=product, product_qty=product_qty)
+        response = JsonResponse({'qty': product_qty})
+        return response
 
