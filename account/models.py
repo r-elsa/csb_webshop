1da060b86a841283be9937fad5721a481c9c592a
r-elsa <elsaryynanen@outlook.com>
2024-08-26 20:39:57 +0300

    adding user amagement

---
+++ account/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+# Create your models here.
+
+from django.contrib.auth.models import (AbstractBaseUser, BaseUserManager, PermissionsMixin)
+
+class UserBase(AbstractBaseUser, PermissionsMixin):
+    email=models.EmailField(_('email address'), unique=True)
+    user_name=models.CharField(max_length=150, unique=True)
+    is_active= models.BooleanField(default=False)
+
+    class Meta:
+        verbose_name= "Accounts"
+        verbose_name_plural= "Accounts"
+
+    def __str__(self):
+        return self.user_name
+
+class CustomAccountManager(BaseUserManager):
+    def create_superuser(self, email, user_name, password, **other_fields):
+    
+        other_fields.setdefault('is_staff', True)
+        other_fields.setdefault('is_superuser', True)
+        other_fields.setdefault('is_active', True)
+
