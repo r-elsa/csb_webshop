efa56608b4d0be6656ecfbb48a573bad47ce0964
r-elsa <elsaryynanen@outlook.com>
2024-08-26 19:39:33 +0300

    basket summary works

--- basket/basket.py
+++ basket/basket.py
@@ -1,3 +1,6 @@
+from django.shortcuts import render, get_object_or_404
+
+from store.models import Product
 
 class Basket ():
     def __init__(self, request):
@@ -10,9 +13,30 @@ class Basket ():
 
     
     def add(self, product, product_qty):
+
         product_id= product.id
 
-        if product_id not in self.basket:
-            self.basket[product_id]= {'price': str(product.price), 'qty': int(product_qty)}
+        self.basket[str(product_id)] = {'price': str(product.price), 'qty': int(product_qty)}
+        
+        print("after", self.basket)
         
         self.session.modified = True
+
+    
+    def __len__(self):
+        return sum(item['qty'] for item in self.basket.values() )
+    
+    def get_items(self):
+        items = []
+        for product_id, data in self.basket.items():
+            product = get_object_or_404(Product, id=product_id)
+            items.append({'product': product, 'qty': data['qty'], 'price': data['price']})
+        return items
+    
+    def get_total_price(self):
+        return sum(float(item['price']) * int(item['qty']) for item in self.basket.values())
+
+    
+
+
+
--- basket/views.py
+++ basket/views.py
@@ -3,18 +3,18 @@ from django.http import JsonResponse
 from .basket import Basket
 from store.models import Product
 
-# Create your views here.
-
 def basket_summary(request):
-    return render(request, 'store/basket/summary.html')
+    basket = Basket(request)
+    basket_items = basket.get_items()  # One-liner to get basket items
+    return render(request, 'store/basket/summary.html', {'basket_items': basket_items})
 
 def basket_add(request):
     basket = Basket(request)
     if request.POST.get('action') == 'post':
         product_id = int(request.POST.get('product_id'))
-        product_qty = int(request.POST.get('product_qty')) 
+        product_qty = int(request.POST.get('product_qty'))
         product = get_object_or_404(Product, id=product_id)
         basket.add(product=product, product_qty=product_qty)
-        response = JsonResponse({'qty': product_qty})
+        basket_qty = len(basket)  # Use the __len__ method to get basket quantity
+        response = JsonResponse({'qty': basket_qty})
         return response
-
